b5a2ae27e6903acf47a34999bde70aa461607276
Goulven Le Dû <goulven.ledu@bappli.com>
2017-06-14 11:58:26 +0200

    FIX Do not validate PHP superglobal variables

--- ITRocks/Sniffs/Variables/Valid_Variable_Name_Sniff.php
+++ ITRocks/Sniffs/Variables/Valid_Variable_Name_Sniff.php
@@ -10,6 +10,24 @@ use PHP_CodeSniffer\Sniffs\Sniff;
 class Valid_Variable_Name_Sniff implements Sniff
 {
 
+	//----------------------------------------------------------------------------------- $white_list
+	/**
+	 * List of variable names that should not be validated.
+	 *
+	 * @var array
+	 */
+	public $white_list = [
+		'$GLOBALS',
+		'$_SERVER',
+		'$_GET',
+		'$_POST',
+		'$_FILES',
+		'$_COOKIE',
+		'$_SESSION',
+		'$_REQUEST',
+		'$_ENV',
+	];
+
 	//----------------------------------------------------------------------------------- isSnakeCase
 	/**
 	 * Returns whether the variable is in snake_case.
@@ -31,7 +49,7 @@ class Valid_Variable_Name_Sniff implements Sniff
 		$tokens = $phpcs_file->getTokens();
 		$variable_name = $tokens[$stack_ptr]['content'];
 
-		if (!$this->isSnakeCase($variable_name)) {
+		if (!in_array($variable_name, $this->white_list) && !$this->isSnakeCase($variable_name)) {
 			$error_message = sprintf('Variable %s is not in valid snake case format.', $variable_name);
 			$phpcs_file->addError($error_message, $stack_ptr, 'Invalid');
 		}
