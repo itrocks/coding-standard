5ff8de2fef8740559797ad1b0519b8b0f8008906
Goulven Le Dû <goulven.ledu@bappli.com>
2017-06-14 15:47:58 +0200

    ADD Creation of a sniff for use statements

---
+++ ITRocks/Sniffs/Classes/User_Order_Sniff.php
@@ -0,0 +1,57 @@
+<?php
+namespace ITRocks\Coding_Standard\Sniffs\Classes;
+
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
+
+/**
+ * Class User_Order_Sniff.
+ */
+class User_Order_Sniff implements Sniff
+{
+
+	//--------------------------------------------------------------------------------------- process
+	/**
+	 * Make sure use statements are alphabetically ordered.
+	 * {@inheritdoc}
+	 */
+	public function process(File $phpcs_file, $stack_ptr)
+	{
+		$previous     = '';
+		$tokens       = $phpcs_file->getTokens();
+		$use_position = $stack_ptr;
+		$very_end     = $phpcs_file->findNext(T_CLASS, $use_position+1);
+
+		while($use_position && $use_position < $very_end) {
+			$end_of_line   = $phpcs_file->findNext(T_SEMICOLON, $use_position+1);
+			$use_statement = '';
+
+			for ($i = $use_position+1; $i < $end_of_line; $i++) {
+				$use_statement .= $tokens[$i]['content'];
+			}
+
+			// Remove potential comments.
+			$use_statement = trim(preg_replace('#(/\*\*?.+\*/)#sU', '', $use_statement));
+
+			if ($previous > $use_statement) {
+				$error_message = sprintf('Use statement "%s" should be before "%s"', $use_statement, $previous);
+				$phpcs_file->addError($error_message, $use_position, 'invalid');
+			}
+
+			$previous     = $use_statement;
+			$use_position = $phpcs_file->findNext(T_USE, $use_position+1);
+		}
+	}
+
+	//-------------------------------------------------------------------------------------- register
+	/**
+	 * {@inheritdoc}
+	 */
+	public function register()
+	{
+		return [
+			T_USE,
+		];
+	}
+
+}
