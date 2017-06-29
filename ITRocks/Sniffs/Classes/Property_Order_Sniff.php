1504eedc2343b5d461511c28a3325d5338af661a
Goulven Le Dû <goulven.ledu@bappli.com>
2017-06-29 12:49:36 +0200

    IMP Check class properties order independently of their type

--- ITRocks/Sniffs/Classes/Property_Order_Sniff.php
+++ ITRocks/Sniffs/Classes/Property_Order_Sniff.php
@@ -11,9 +11,10 @@ use PHP_CodeSniffer\Sniffs\Sniff;
  */
 class Property_Order_Sniff implements Sniff
 {
+
 	//--------------------------------------------------------------------------------------- process
 	/**
-	 * Process this test, when one of its token is encountered.
+	 * Make sure class properties are ordered alphabetically.
 	 *
 	 * @param $phpcs_file File : The file being scanned.
 	 * @param $stack_ptr  int  : The position of the current token.
@@ -21,16 +22,18 @@ class Property_Order_Sniff implements Sniff
 	 */
 	public function process(File $phpcs_file, $stack_ptr)
 	{
-		$tokens = $phpcs_file->getTokens();
-
-		$wanted_tokens = [T_PUBLIC, T_PROTECTED, T_PRIVATE];
-
-		$scope = $phpcs_file->findNext($wanted_tokens, $stack_ptr);
+		$wanted_tokens     = [T_PUBLIC, T_PROTECTED, T_PRIVATE, T_STATIC];
 		$previous_property = '';
+		$scope             = $phpcs_file->findNext($wanted_tokens, $stack_ptr);
+		$tokens            = $phpcs_file->getTokens();
 
 		while($scope) {
-			if ($tokens[$scope + 2]['code'] == T_VARIABLE) {
-				$property = $tokens[$scope + 2]['content'];
+			if ($tokens[$scope + 2]['code'] == T_VARIABLE || $tokens[$scope + 4]['code'] == T_VARIABLE) {
+				if ($tokens[$scope + 2]['code'] == T_VARIABLE) {
+					$property = $tokens[$scope + 2]['content'];
+				} else {
+					$property = $tokens[$scope + 4]['content'];
+				}
 
 				if ($property < $previous_property) {
 					$err_msg = sprintf('Property %s must be declared before %s.', $property, $previous_property);
