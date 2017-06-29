7f3d9b2ed72d3b0ab042db89c9b800bb290ab8ec
Goulven Le Dû <goulven.ledu@bappli.com>
2017-06-29 12:18:24 +0200

    ADD Creation of a new sniff to check class constants' order

---
+++ ITRocks/Sniffs/Classes/Const_Order_Sniff.php
@@ -0,0 +1,54 @@
+<?php
+namespace ITRocks\Coding_Standard\Sniffs\Classes;
+
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
+
+/**
+ * Class Const_Order_Sniff.
+ */
+class Const_Order_Sniff implements Sniff
+{
+
+	//--------------------------------------------------------------------------------------- process
+	/**
+	 * Make sure class's constants are alphabetically ordered.
+	 *
+	 * {@inheritdoc}
+	 */
+	public function process(File $phpcs_file, $stack_ptr)
+	{
+		$const_position = $phpcs_file->findNext(T_CONST, $stack_ptr + 1);
+		$previous       = '';
+		$tokens         = $phpcs_file->getTokens();
+
+		while ($const_position) {
+			$constant = $tokens[$const_position + 2]['content'];
+
+			if ($constant < $previous) {
+				$error_message = sprintf(
+					'Constant %s must declared before constant %s',
+					$constant,
+					$previous
+				);
+
+				$phpcs_file->addError($error_message, $const_position, 'invalid');
+			}
+
+			$const_position = $phpcs_file->findNext(T_CONST, $const_position + 1);
+			$previous       = $constant;
+		}
+	}
+
+	//-------------------------------------------------------------------------------------- register
+	/**
+	 * {@inheritdoc}
+	 */
+	public function register()
+	{
+		return [
+			T_CLASS,
+		];
+	}
+
+}
