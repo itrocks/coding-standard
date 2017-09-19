d4872c1a9b0726a296ca7b77232f007312b542bd
Goulven Le Dû <goulven.ledu@bappli.com>
2017-09-19 18:15:44 +0200

    IMP 97393 Make sure there is no blank line between annotations

--- ITRocks/Sniffs/Comments/Annotations_Sniff.php
+++ ITRocks/Sniffs/Comments/Annotations_Sniff.php
@@ -10,6 +10,39 @@ use PHP_CodeSniffer\Sniffs\Sniff;
 class Annotations_Sniff implements Sniff
 {
 
+	//-------------------------------------------------------------------------------- findBlankLines
+	/**
+	 * Returns true if there is at least one blank line between 2 annotations.
+	 *
+	 * @param $tokens array   The token list.
+	 * @param $start  integer Where to start search for.
+	 * @param $stop   integer Where to stop search for.
+	 * @return bool
+	 */
+	private function findBlankLines(array $tokens, $start, $stop)
+	{
+		$current_line = $tokens[$stop]['line'];
+		$line         = '';
+
+		for ($i = $start; $i < $stop; $i++) {
+			$item = $tokens[$i];
+
+			if ($item['line'] == $current_line) {
+				$line .= $item['content'];
+			}
+			else {
+				if (trim($line) === '*') {
+					return true;
+				}
+
+				$current_line = $item['line'];
+				$line         = '';
+			}
+		}
+
+		return false;
+	}
+
 	//--------------------------------------------------------------------------------------- process
 	/**
 	 * {@inheritdoc}
@@ -27,6 +60,16 @@ class Annotations_Sniff implements Sniff
 				$phpcs_file->addError(
 					'Annotations must be ordered alphabetically',
 					$stack_ptr,
+					'Invalid'
+				);
+			}
+
+			if ($tokens[$next]['line'] - $tokens[$stack_ptr]['line'] != 1
+				&& $this->findBlankLines($tokens, $stack_ptr, $next)
+			) {
+				$phpcs_file->addError(
+					'There must be no blank lines between annotations',
+					$next,
 					'Invalid'
 				);
 			}
