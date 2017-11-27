4db3eb8e6e102a2ede21752a977cfe914c6337d0
Goulven Le Dû <goulven.ledu@bappli.com>
2017-11-27 10:01:52 +0100

    TST 97393 Unit test function comment sniff

---
+++ ITRocks/Tests/Comments/Function_Comment_Sniff_Test.php
@@ -0,0 +1,38 @@
+<?php
+namespace ITRocks\Coding_Standard\Tests\Comments;
+
+use ITRocks\Coding_Standard\Sniffs\Comments\Function_Comment_Sniff;
+use ITRocks\Coding_Standard\Tests\Sniff_Test_Case;
+
+/**
+ * Class Function_Comment_Test.
+ */
+class Function_Comment_Sniff_Test extends Sniff_Test_Case
+{
+
+	//----------------------------------------------------------------------------------------- SNIFF
+	const SNIFF = Function_Comment_Sniff::class;
+
+	//----------------------------------------------------------------------------- getExpectedErrors
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getExpectedErrors()
+	{
+		return [
+			6  => [
+				[
+					'source'  => 'Coding_Standard.Comments.Function_Comment_.Invalid',
+					'message' => 'PHPdoc param must begin with the variable name followed by its type',
+				],
+			],
+			18 => [
+				[
+					'source'  => 'Coding_Standard.Comments.Function_Comment_.Invalid',
+					'message' => 'PHPdoc param must begin with the variable name followed by its type',
+				]
+			],
+		];
+	}
+
+}
