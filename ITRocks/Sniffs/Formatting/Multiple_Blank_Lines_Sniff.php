22481bb9f828607e523cb9c6ad8935fed82df8c6
Goulven Le Dû <goulven.ledu@bappli.com>
2018-03-14 15:34:34 +0100

    REF 101230 Search for blank lines in doc comments too

--- ITRocks/Sniffs/Formatting/Multiple_Blank_Lines_Sniff.php
+++ ITRocks/Sniffs/Formatting/Multiple_Blank_Lines_Sniff.php
@@ -12,25 +12,54 @@ use PHP_CodeSniffer\Sniffs\Sniff;
 class Multiple_Blank_Lines_Sniff implements Sniff
 {
 
+	//------------------------------------------------------------------- contiguousBlankLinesToError
+	/**
+	 * Add error on contiguous blank lines.
+	 *
+	 * @param $file        File  The parsed file
+	 * @param $blank_lines array The blank lines found in file
+	 */
+	private function contiguousBlankLinesToError(File $file, array $blank_lines)
+	{
+		$error  = false;
+		$tokens = $file->getTokens();
+
+		foreach ($blank_lines as $line => $token) {
+			// If there is a blank line right after the current one.
+			if (isset($blank_lines[$line+1])) {
+				$error = true;
+			} else {
+				if ($error) {
+					// The current line is the last one of a group of blank lines.
+					$position = array_search($token, $tokens);
+					$file->addError('Multiple blank lines are not allowed', $position, 'invalid');
+				}
+
+				// Reset error flag.
+				$error = false;
+			}
+		}
+	}
+
 	//-------------------------------------------------------------------------------- findBlankLines
 	/**
-	 * Find all blank lines in the given tokens.
+	 * Find all blank lines in the given token list.
+	 * Blank lines in comment doc are looked for too.
 	 *
 	 * @param $tokens     array
 	 * @param $white_list array
 	 * @return array
 	 */
-	private function findBlankLines(array $tokens, $white_list = [T_WHITESPACE])
-	{
+	private function findBlankLines(
+		array $tokens, $white_list = [T_WHITESPACE, T_DOC_COMMENT_WHITESPACE, T_DOC_COMMENT_STAR]
+	) {
 		$blank_lines = [];
-		$ignored = [];
+		$ignored     = [];
 
 		foreach ($tokens as $token) {
 			if (!in_array($token['line'], $ignored)) {
 				if (in_array($token['code'], $white_list)) {
-					if (!in_array($token['line'], $blank_lines)) {
-						$blank_lines[$token['line']] = $token;
-					}
+					$blank_lines[$token['line']] = $token;
 				}
 				else {
 					$ignored[] = $token['line'];
@@ -48,25 +77,9 @@ class Multiple_Blank_Lines_Sniff implements Sniff
 	 */
 	public function process(File $file, $stack_ptr)
 	{
-		$error  = false;
-		$tokens = $file->getTokens();
-		$lines  = $this->findBlankLines($tokens);
+		$blank_lines = $this->findBlankLines($file->getTokens());
 
-		foreach ($lines as $line => $token) {
-			// If there is a blank line right after the current one.
-			if (isset($lines[$line+1])) {
-				$error = true;
-			} else {
-				if ($error) {
-					// The current line is the last one of a group of blank lines.
-					$position = array_search($token, $tokens);
-					$file->addError('Multiple blank lines are not allowed', $position, 'invalid');
-				}
-
-				// Reset error flag.
-				$error = false;
-			}
-		}
+		$this->contiguousBlankLinesToError($file, $blank_lines);
 	}
 
 	//-------------------------------------------------------------------------------------- register
