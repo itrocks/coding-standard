503d828784bca4ff7cde1220ad52d2698fdc5230
jean.ledrogo <jean.ledrogo@bappli.com>
2021-05-28 18:12:40 +0200

    ADD 179712 implement control and fix on return type coding standard

--- ITRocks/Sniffs/Functions/Opening_Function_Brace_Sniff.php
+++ ITRocks/Sniffs/Functions/Opening_Function_Brace_Sniff.php
@@ -14,8 +14,21 @@ class Opening_Function_Brace_Sniff extends OpeningFunctionBraceBsdAllmanSniff
 	//------------------------------------------------------------------------------- ERROR_SAME_LINE
 	const ERROR_SAME_LINE = 'AutoFixable : Opening brace must be on the same line as closing parenthesis';
 
+	//------------------------------------------------------------------------------ ERROR_SAME_LINE2
+	const ERROR_SAME_LINE2 = 'AutoFixable : Opening brace must be on next line when having multiple line function definition with a return type' ;
+
+	//------------------------------------------------------------------------------ ERROR_SAME_LINE3
+	const ERROR_SAME_LINE3 = 'AutoFixable : Closing parenthesis must return to new line when having multiple line function definition';
+
 	//--------------------------------------------------------------------------------- ERROR_SPACING
 	const ERROR_SPACING = 'AutoFixable : There must be one single space between closing parenthesis and opening brace';
+
+	//-------------------------------------------------------------------------------- ERROR_SPACING2
+	const ERROR_SPACING2 = 'AutoFixable : There must be one single space between return type separator and return type';
+
+	//-------------------------------------------------------------------------------- ERROR_SPACING3
+	const ERROR_SPACING3 = 'AutoFixable : There must be one single space between closing parenthesis and return type separator';
+
 
 	//--------------------------------------------------------------------------------------- process
 	/**
@@ -24,6 +37,7 @@ class Opening_Function_Brace_Sniff extends OpeningFunctionBraceBsdAllmanSniff
 	public function process(File $file, $stack_ptr)
 	{
 		$tokens          = $file->getTokens();
+		$a_function_name = $tokens[$stack_ptr+2]['content'] ; // For debugging purpose
 		$token_navigator = new Token_Navigator($file, $stack_ptr);
 
 		// Does not exist for function taking no parameters.
@@ -34,29 +48,100 @@ class Opening_Function_Brace_Sniff extends OpeningFunctionBraceBsdAllmanSniff
 		$parenthesis_opener = $tokens[$stack_ptr]['parenthesis_opener'];
 		$parenthesis_closer = $tokens[$stack_ptr]['parenthesis_closer'];
 
-		if ($tokens[$parenthesis_closer]['line'] != $tokens[$parenthesis_opener]['line']
-			&& isset($brace_opener)
-		) {
-			// Opening brace must be on the same line as closing parenthesis.
-			if ($tokens[$brace_opener]['line'] != $tokens[$parenthesis_closer]['line']) {
-				$fix = $file->addFixableError(self::ERROR_SAME_LINE, $stack_ptr, 'Invalid');
-				if ($fix) {
-					$file->fixer->replaceToken($brace_opener, ' {');
-					$token_navigator->clean($parenthesis_closer + 1, $brace_opener - 1);
+		if(isset($brace_opener)){
+			$return_type_separator = null ;
+			for($i=$parenthesis_closer+1 ; $i < $brace_opener ; $i++){
+				if($tokens[$i]['content'] == ':'){
+					$return_type_separator = $i ;
+					break;
 				}
 			}
-			elseif ($brace_opener !== ($parenthesis_closer + 2)
-				|| $tokens[($parenthesis_closer + 1)]['code'] != T_WHITESPACE
-				|| $tokens[($parenthesis_closer + 1)]['length'] != 1
-			) {
-				$fix = $file->addFixableError(self::ERROR_SPACING, ($parenthesis_closer + 1), 'Invalid');
-				if ($fix) {
-					$file->fixer->replaceToken($brace_opener, ' {');
-					$token_navigator->clean($parenthesis_closer + 1, $brace_opener - 1);
+
+			if ($return_type_separator) {
+				// Spacing ") :"
+				if ($tokens[$parenthesis_closer + 1]['type'] != "T_WHITESPACE" || $tokens[$return_type_separator + 1]['length'] != 1) {
+					$fix = $file->addFixableError(self::ERROR_SPACING3, $stack_ptr, 'Invalid');
+					if($fix){
+						for ($i = $parenthesis_closer + 1; $i < $return_type_separator; $i++) {
+								$token_navigator->clean($i, $i);
+						}
+						$file->fixer->addContent($parenthesis_closer, ' ');
+					}
+				}
+
+				// Spacing ": returntype"
+				if ($tokens[$return_type_separator + 1]['type'] != "T_WHITESPACE" || $tokens[$return_type_separator + 1]['length'] != 1) {
+					$fix = $file->addFixableError(
+						self::ERROR_SPACING2, $return_type_separator + 1, 'Invalid'
+					);
+					if ($fix) {
+						for ($i = $return_type_separator + 1; $i < $brace_opener - 2; $i++) {
+							if ($tokens[$i]['type'] == "T_WHITESPACE" && $tokens[$i]['content'] != "\n") {
+								$token_navigator->clean($i, $i);
+							}
+						}
+						$file->fixer->addContent($return_type_separator, ' ');
+					}
+				}
+
+				// Opening brace must be on next line when having a return type
+				if ($tokens[$brace_opener]['line'] != $tokens[$return_type_separator]['line'] + 1) {
+					$fix = $file->addFixableError(self::ERROR_SAME_LINE2, $stack_ptr, 'Invalid');
+					if ($fix) {
+						// Clean spacing
+						for ($i = $brace_opener - 1; ; $i--) {
+							if ($tokens[$i]['type'] == "T_WHITESPACE") {
+								$token_navigator->clean($i, $i);
+							}
+							else {
+								break;
+							}
+						}
+						// And go to next line
+						$file->fixer->addNewlineBefore($brace_opener);
+					}
+				}
+			}
+			else {
+				// No return type
+				if ($tokens[$parenthesis_closer]['line'] != $tokens[$parenthesis_opener]['line']) {
+					// Multiple line function declaration
+					// Closing parenthesis must return to new line
+					if ($tokens[$parenthesis_closer - 1]['type'] != "T_WHITESPACE") {
+						$fix = $file->addFixableError(self::ERROR_SAME_LINE3, $stack_ptr, 'Invalid');
+						if ($fix) {
+							$file->fixer->addNewlineBefore($parenthesis_closer);
+						}
+					}
+					// Opening brace must be on the same line as closing parenthesis when no return type
+					if ($tokens[$brace_opener]['line'] != $tokens[$parenthesis_closer]['line']) {
+						$fix = $file->addFixableError(self::ERROR_SAME_LINE, $stack_ptr, 'Invalid');
+						if ($fix) {
+							$file->fixer->replaceToken($brace_opener, ' {');
+							$token_navigator->clean($parenthesis_closer + 1, $brace_opener - 1);
+						}
+					}
+					elseif ($brace_opener !== ($parenthesis_closer + 2)
+						|| $tokens[($parenthesis_closer + 1)]['code'] != T_WHITESPACE
+						|| $tokens[($parenthesis_closer + 1)]['length'] != 1
+					) {
+						$fix = $file->addFixableError(
+							self::ERROR_SPACING, ($parenthesis_closer + 1), 'Invalid'
+						);
+						if ($fix) {
+							$file->fixer->replaceToken($brace_opener, ' {');
+							$token_navigator->clean($parenthesis_closer + 1, $brace_opener - 1);
+						}
+					}
+				}
+				else {
+					// Single line function without return type
+					parent::process($file, $stack_ptr);
 				}
 			}
 		}
 		else {
+			// No brace opener
 			parent::process($file, $stack_ptr);
 		}
 	}
--- ITRocks/Tests/Functions/Opening_Function_Brace_Test.php
+++ ITRocks/Tests/Functions/Opening_Function_Brace_Test.php
@@ -15,6 +15,18 @@ class Opening_Function_Brace_Test extends Sniff_Test_Case
 	//---------------------------------------------------------------------------------------- SOURCE
 	const SOURCE = 'Coding_Standard.Functions.Opening_Function_Brace_.Invalid';
 
+	//--------------------------------------------------------------------------------------- SOURCE2
+	const SOURCE2 = 'Coding_Standard.Functions.Opening_Function_Brace_.BraceOnSameLine';
+
+	//--------------------------------------------------------------------------------------- SOURCE3
+	const SOURCE3 = 'Coding_Standard.Functions.Opening_Function_Brace_.BraceSpacing';
+
+	//--------------------------------------------------------------------------------------- SOURCE4
+	const SOURCE4 = 'Coding_Standard.Formatting.Multiple_Blank_Lines_.Invalid';
+
+	//--------------------------------------------------------------------------------------- SOURCE5
+	const SOURCE5 = 'Coding_Standard.Comments.Comment_Separator_.Invalid';
+
 	//----------------------------------------------------------------------------- getExpectedErrors
 	/**
 	 * @return Error[]
@@ -23,9 +35,27 @@ class Opening_Function_Brace_Test extends Sniff_Test_Case
 	public function getExpectedErrors()
 	{
 		return [
-			NEW Error(22, Opening_Function_Brace_Sniff::ERROR_SAME_LINE, static::SOURCE),
-			NEW Error(44, Opening_Function_Brace_Sniff::ERROR_SPACING, static::SOURCE),
-			NEW Error(54, Opening_Function_Brace_Sniff::ERROR_SPACING, static::SOURCE),
+			NEW Error(22, 'Opening brace should be on a new line', static::SOURCE2),
+			NEW Error(32, 'Opening brace should be on the line after the declaration; found 1 blank line(s)', static::SOURCE3),
+			NEW Error(49, 'Opening brace should be on a new line', static::SOURCE2),
+			NEW Error(59, 'AutoFixable : Multiple blank lines are not allowed', static::SOURCE4),
+			NEW Error(60, 'Opening brace should be on the line after the declaration; found 2 blank line(s)', static::SOURCE3),
+			NEW Error(78, Opening_Function_Brace_Sniff::ERROR_SAME_LINE3, static::SOURCE),
+			NEW Error(79, Opening_Function_Brace_Sniff::ERROR_SPACING, static::SOURCE),
+			NEW Error(89, Opening_Function_Brace_Sniff::ERROR_SPACING, static::SOURCE),
+			NEW Error(99, Opening_Function_Brace_Sniff::ERROR_SPACING, static::SOURCE),
+			NEW Error(108, Opening_Function_Brace_Sniff::ERROR_SAME_LINE, static::SOURCE),
+			NEW Error(128, Opening_Function_Brace_Sniff::ERROR_SAME_LINE2, static::SOURCE),
+			NEW Error(136, Opening_Function_Brace_Sniff::ERROR_SAME_LINE2, static::SOURCE),
+			NEW Error(146, Opening_Function_Brace_Sniff::ERROR_SPACING3, static::SOURCE),
+			NEW Error(155, Opening_Function_Brace_Sniff::ERROR_SPACING3, static::SOURCE),
+			NEW Error(155, Opening_Function_Brace_Sniff::ERROR_SPACING2, static::SOURCE),
+			NEW Error(182, Opening_Function_Brace_Sniff::ERROR_SPACING3, static::SOURCE),
+			NEW Error(182, Opening_Function_Brace_Sniff::ERROR_SPACING2, static::SOURCE),
+			NEW Error(236, Opening_Function_Brace_Sniff::ERROR_SAME_LINE2, static::SOURCE),
+			NEW Error(247, Opening_Function_Brace_Sniff::ERROR_SAME_LINE2, static::SOURCE),
+			NEW Error(270, 'AutoFixable : Comment separator for function case091complexWithReturnType() is invalid', static::SOURCE5),
+			NEW Error(270, Opening_Function_Brace_Sniff::ERROR_SAME_LINE2, static::SOURCE),
 		];
 	}
 
