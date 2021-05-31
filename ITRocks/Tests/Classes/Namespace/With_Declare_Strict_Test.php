9a7d067bbbf59a46ecf589cc8c50d3cd3bd8d471
jean.ledrogo <jean.ledrogo@bappli.com>
2021-05-31 10:49:55 +0200

    ADD 000000 implement test for declare_strict syntax

---
+++ ITRocks/Tests/Classes/Namespace/With_Declare_Strict_Test.php
@@ -0,0 +1,29 @@
+<?php
+namespace ITRocks\Coding_Standard\Tests\Classes\Namespaces;
+
+use ITRocks\Coding_Standard\Sniffs\Classes\Namespace_Sniff;
+use ITRocks\Coding_Standard\Tests\Error;
+use ITRocks\Coding_Standard\Tests\Sniff_Test_Case;
+
+/**
+ * Test case where using declare(strict_types=1);
+ *
+ * @see Namespace_Sniff
+ */
+class With_Declare_Strict_Test extends Sniff_Test_Case
+{
+	//---------------------------------------------------------------------------------------- SOURCE
+	const SOURCE = 'Coding_Standard.Classes.Namespace_.Invalid';
+
+	//----------------------------------------------------------------------------- getExpectedErrors
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getExpectedErrors()
+	{
+		return [
+			new Error(6, Namespace_Sniff::ERROR_LINE.'2', static::SOURCE),
+		];
+	}
+
+}
